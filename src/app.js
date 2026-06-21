3804c3be396ee1e11237c026d907d7a45ee7796e
romanticahq <admin@romanticahq.com>
2026-06-21 14:25:33 +0100

    Support admin image uploads

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "12mb" }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
